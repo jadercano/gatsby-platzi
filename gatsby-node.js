79762bbcd91d4132b7e9c13b376dafae47360ddb
Jader Cano <jader.cano@contractors.accruent.com>
2021-07-11 08:17:04 -0500

    Added functionality to create Product Pages using Gatsby.

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,41 @@
  */
 
 // You can delete this file if you're not using it
+const path = require("path")
+
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
+  const productTemplate = path.resolve("src/templates/product.js")
+  const result = await graphql(`
+    query GET_PRODUCTS {
+      allStripePrice {
+        edges {
+          node {
+            currency
+            unit_amount
+            product {
+              id
+              name
+              metadata {
+                description
+                img
+                wear
+              }
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  if (result.errors) {
+    throw result.errors
+  }
+
+  result.data.allStripePrice.edges.forEach(({ node }) => {
+    createPage({
+      path: `${node.product.id}`,
+      component: productTemplate,
+    })
+  })
+}
